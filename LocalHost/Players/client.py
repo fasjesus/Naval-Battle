6b37fc93e4d627ffb51c22d76e03eeb67260ac01
fasjesus <developerflavia@gmail.com>
2024-07-03 16:56:46 -0300

    notas

---
+++ LocalHost/Players/client.py
@@ -0,0 +1,19 @@
+import socket
+
+def main():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('127.0.0.1', 8080))
+
+    while True:
+        data = client.recv(1024).decode('utf-8')
+        print(data)
+        if "Fim de jogo" in data:
+            break
+        if "Sua vez!" in data:
+            letra = input("Digite a letra: ")
+            numero = input("Digite o número: ")
+            client.sendall(f"{letra} {numero}".encode('utf-8'))
+    client.close()
+
+if __name__ == "__main__":
+    main()
