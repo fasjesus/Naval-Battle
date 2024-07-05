258df38401576159492c468d64ee844987b0af97
fasjesus <developerflavia@gmail.com>
2024-07-05 13:34:04 -0300

    version final

--- Aplication/Single/server.py
+++ Aplication/Single/server.py
@@ -102,7 +102,7 @@ def handle_client(conn, addr):
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(('0.0.0.0', 8080))
-    server.listen(5)
+    server.listen(1)
     print("Servidor iniciado na porta 8080")
     while True:
         conn, addr = server.accept()
